a070d27986c59c5c0f7d173a75d340954c453643
Descobedo <diegovescobedo@gmail.com>
2018-03-17 00:25:29 -0500

    fixed keen, removed some code

--- botStart.py
+++ botStart.py
@@ -11,11 +11,6 @@ client = commands.Bot(command_prefix=bot_prefix)
 @client.event
 async def on_ready():
     print("Waf Ready!")
-    #print("Name: {}".fromat(client.user.name))
-    #print("ID: {}".format(client.user.id))
-@client.command(pass_context=True)
-async def _func(ctx):
-    await client.say("Teacher Boy\nOwner of pair o' dice\n ")
 @client.command(pass_context=True)
 async def perritopan(ctx):
     await client.say("Tall Boy\nDave Franco esque\n ")
@@ -35,7 +30,7 @@ async def Kevdawg(ctx):
 async def n98(ctx):
     await client.say("Full Scholarship Boy\n Spy for the government\n ")
 @client.command(pass_context=True)
-async def Chrutercraft(ctx):
+async def Chruftercraft(ctx):
     await client.say("Gnome Boy\n*power drill*\n")
 @client.command(pass_context=True)
 async def Chapparal(ctx):
